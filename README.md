68a745829cf0b0363d4370f0854f1926d5b78d61
marcoguastalli <marco.guastalli@gmail.com>
2019-02-04 16:44:49 +0100

    add mongoose

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ Express App
 npm init
 npm install --save express body-parser mongoose
 node app.js
+
+http://localhost:1234/products/test
+
+
+
+### run
+mongod --dbpath /Users/marcoguastalli/opt/MongoDB/m27data/db --auth
+
+### login
+mongo -u local -p local 127.0.0.1:27017 --authenticationDatabase admin
+
+db.createCollection("expressapp")
--- app.js
+++ app.js
@@ -2,14 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const product = require('./routes/product.route'); // Imports routes for the products
-
-// initialize our express app
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://local:local@localhost:27017/local?authSource=admin", { useNewUrlParser: true });
+
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
+
 
 let port = 1234;
 
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
 });
+
+
