c4afa495e2d69e93795db09d9d67202afe34df52
marcoguastalli <marco.guastalli@gmail.com>
2022-08-29 10:15:03 +0200

    mongoose v6

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const app = express();
 // Set up mongoose connection
 const mongoose = require('mongoose');
 mongoose.connect("mongodb://local:local@localhost:27017/local?authSource=admin", {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
+    autoIndex: false, // Don't build indexes
+    maxPoolSize: 10, // Maintain up to 10 socket connections
+    serverSelectionTimeoutMS: 50000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6
 });
 
 mongoose.Promise = global.Promise;
@@ -25,7 +27,7 @@ app.use('/products', product);
 let port = 1234;
 
 app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
+    console.log('Server is up and running on port ' + port);
 });
 
 
